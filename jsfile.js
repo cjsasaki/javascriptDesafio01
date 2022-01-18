e289d9b89c88c6ff03075cb30e6593af38fb9b99
Cesar Sasaki <cesar.sasaki@gmail.com>
2022-01-18 08:59:16 -0500

    fix factorial 0

--- jsfile.js
+++ jsfile.js
@@ -1,7 +1,7 @@
 let numero = prompt("Ingrese valor de un número para el cálculo factorial");
 let factorial = 1;
 if (numero == 0)  {
-    factorial = 0;
+    factorial = 1;
 }else {
     for (let i=1 ; i<=numero;i++){
         factorial = factorial*i;
